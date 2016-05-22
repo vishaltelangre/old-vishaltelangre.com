594f686841b6a74eedf07b5b970143dd3a0bd958
Vishal Telangre <the@vishaltelangre.com>
2016-05-22 20:47:28 +0530

    Move index.html to static directory

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-var, strict */
 
+var path = require('path');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
 var devServer = new WebpackDevServer(webpack(config), {
+  contentBase: path.join(__dirname, 'static'),
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
