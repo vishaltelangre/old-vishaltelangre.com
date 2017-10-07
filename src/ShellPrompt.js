10dc185fd32ab374f8d5048c7c4858e58738ac14
Vishal Telangre <the@vishaltelangre.com>
2017-10-07 18:49:53 +0530

    Cleanup React stuff

--- src/ShellPrompt.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default class ShellPrompt extends Component {
-  render() {
-    return (
-      <span style={{color: '#f00'}}>> </span>
-    );
-  }
-}
