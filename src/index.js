66f7f6018bcf151c31e1a3350ddda1c7de3c5df3
Vishal Telangre <the@vishaltelangre.com>
2017-10-08 13:53:01 +0530

    Always keep focus in the prompt

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import './main.css';
 import { Main } from './Main.elm';
 
 Main.embed(document.getElementById('root'));
+
+// Keep focus in the prompt on pressing Tab key
+document.querySelector('body').addEventListener('keydown', (evt) => {
+  evt = evt || event;
+  if (evt.keyCode === 9 /* Tab key */) {
+    evt.preventDefault();
+    document.querySelector('#prompt').focus();
+  }
+});
+
+// Keep focus in the prompt on clicking anywhere on the page
+document.querySelector('*').addEventListener('click', (event) => {
+  document.querySelector('#prompt').focus();
+});
